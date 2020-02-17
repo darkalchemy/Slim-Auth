7574a2687fb95ac3292acaaad70801926491972a
darkalchemy <darkalchemy@jbmatrix.net>
2020-02-17 18:27:14 -0500

    add: remove trailing slash middleware

--- bootstrap/middleware.php
+++ bootstrap/middleware.php
@@ -7,6 +7,7 @@ use App\Middleware\FlashOldFormDataMiddleware;
 use App\Middleware\SessionMiddleware;
 use App\Middleware\SetLocaleMiddleware;
 use Illuminate\Database\Capsule\Manager as Capsule;
+use Middlewares\TrailingSlash;
 use Slim\App;
 use Slim\Csrf\Guard;
 use Slim\Views\TwigMiddleware;
@@ -24,4 +25,5 @@ return function (App $app) {
     $app->addMiddleware($container->get(SetLocaleMiddleware::class));
     $app->addMiddleware($container->get(SessionMiddleware::class));
     $app->addRoutingMiddleware();
+    $app->addMiddleware($container->get(TrailingSlash::class));
 };
