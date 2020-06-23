34e99619457a5166f658eaa000d8b22aacc1d2e1
pankaj <pankajbd.com@gmail.com>
2020-06-23 19:16:26 +0600

    fix: unsafe redirect

--- app/Middleware/RedirectIfAuthenticated.php
+++ app/Middleware/RedirectIfAuthenticated.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Middleware;
 
 use Cartalyst\Sentinel\Native\Facades\Sentinel;
+use Nyholm\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
@@ -22,11 +23,11 @@ class RedirectIfAuthenticated
      */
     public function __invoke(ServerRequestInterface $request, RequestHandlerInterface $handler)
     {
-        $response = $handler->handle($request);
         if (!Sentinel::guest()) {
+            $response = new Response();
             return $response->withHeader('Location', '/');
         }
 
-        return $response;
+        return $handler->handle($request);
     }
 }
