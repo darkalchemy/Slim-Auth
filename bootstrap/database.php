2726b98bca8a85fce6f448673244e2b7713070a2
darkalchemy <darkalchemy@jbmatrix.net>
2022-01-23 12:36:30 -0500

    update: remove static from function

--- bootstrap/database.php
+++ bootstrap/database.php
@@ -6,7 +6,7 @@ use Cartalyst\Sentinel\Native\Facades\Sentinel;
 use Cartalyst\Sentinel\Native\SentinelBootstrapper;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-return static function ($settings) {
+return function ($settings) {
     Sentinel::instance(new SentinelBootstrapper((require CONFIG_DIR . 'sentinel.php')));
     $capsule = new Capsule();
     $capsule->addConnection($settings);
